d598f4e092fc5162b9ec7627792868b75fad7337
woshig882019 <50591022+woshig882019@users.noreply.github.com>
2019-05-18 17:39:56 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -227,13 +227,6 @@ install_sprov-ui() {
     if [[ -f "/usr/local/sprov-ui/sprov-ui.war" ]]; then
         rm /usr/local/sprov-ui/sprov-ui.war -f
     fi
-    last_version=$(curl --silent "https://api.github.com/repos/woshig882019/sprov-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-    echo -e "检测到sprov-ui最新版本：${last_version}，开始下载核心文件"
-    wget -N --no-check-certificate -O /usr/local/sprov-ui/sprov-ui.jar https://github.com/woshig882019/sprov-ui/releases/download/${last_version}/sprov-ui-${last_version}.jar
-    if [[ $? -ne 0 ]]; then
-        echo -e "${red}下载sprov-ui核心文件失败，请确保你的服务器能够下载Github的文件，如果多次安装失败，请参考手动安装教程${plain}"
-        exit 1
-    fi
     set_systemd
     echo -e ""
     echo -e "${green}sprov-ui 面板安装成功${plain}\n"
